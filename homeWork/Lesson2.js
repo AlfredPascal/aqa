0f19bc2f1e4e3a4f8f2388402f0b29881ecec307
Alfred Pascal <Alfred.P4sc4l@gmail.com>
2026-03-19 12:56:40 +0300

    Add lesson 2

---
+++ homeWork/Lesson2.js
@@ -0,0 +1,34 @@
+// TO EASY
+
+
+let firstName = "Alex";
+let lastName = "Petroff";
+console.log(firstName + " " + lastName);
+firstName = "Poul";
+lastName = "Walker";
+console.log(firstName + " " + lastName);
+
+/////////////////////////////////////////////////////////
+
+let a;
+let c = 9;
+let str = "Hi 5!";
+let b = true;
+let y = 9 + "1";
+let x = "a" / 6;
+
+console.log(typeof(a));
+console.log(typeof(c));
+console.log(typeof(str));
+console.log(typeof(b));
+console.log(typeof(y));
+console.log(typeof(x));
+
+
+/////////////////////////////////////////////////////////
+
+const ENV = "DEV";
+console.log(ENV);
+
+
+//////////////////////////////////////////////////////
